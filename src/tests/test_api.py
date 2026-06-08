2d6b8920c8cbbfbe351a8e3dfbecb18f8f333234
dillon-goh-tw <dillon.goh@thoughtworks.com>
2026-06-08 18:32:57 +0800

    Refactor test code for improved readability and consistency

--- src/tests/test_api.py
+++ src/tests/test_api.py
@@ -4,6 +4,7 @@ import sys
 
 client = TestClient(app)
 
+
 def test_root():
     response = client.get("/")
     assert response.status_code == 200
--- src/tests/test_service.py
+++ src/tests/test_service.py
@@ -3,38 +3,39 @@ from app.service import TriviaService
 from unittest.mock import patch, AsyncMock
 import pytest
 
+
 @pytest.mark.asyncio
 async def test_fetch_trivia(httpx_mock):
-    mock_response=  {
-            "response_code": 0,
-            "results": [
-                {
-                    "type": "multiple",
-                    "difficulty": "easy",
-                    "category": "General Knowledge",
-                    "question": "What is the name of the Jewish New Year?",
-                    "correct_answer": "Rosh Hashanah",
-                    "incorrect_answers": [
-                    "Elul",
-                    "New Year",
-                    "Succoss"
-                ]
+    mock_response = {
+        "response_code": 0,
+        "results": [
+            {
+                "type": "multiple",
+                "difficulty": "easy",
+                "category": "General Knowledge",
+                "question": "What is the name of the Jewish New Year?",
+                "correct_answer": "Rosh Hashanah",
+                "incorrect_answers": ["Elul", "New Year", "Succoss"],
             }
-        ]
+        ],
     }
-    httpx_mock.add_response(url="https://opentdb.com/api.php?amount=1&type=multiple&category=9", json=mock_response)
-    
+    httpx_mock.add_response(
+        url="https://opentdb.com/api.php?amount=1&type=multiple&category=9",
+        json=mock_response,
+    )
+
     service = TriviaService(base_url="https://opentdb.com/api.php")
     questions = await service.fetch_trivia(amount=1, type="multiple", category=9)
-    
+
     assert questions[0]["question"] == "What is the name of the Jewish New Year?"
     assert questions[0]["correct_answer"] == "Rosh Hashanah"
     assert "Rosh Hashanah" in questions[0]["all_answers"]
     assert len(questions[0]["all_answers"]) == 4
 
+
 @pytest.mark.asyncio
 async def test_fetch_trivia_number_of_questions(httpx_mock):
-    mock_response=  {
+    mock_response = {
         "response_code": 0,
         "results": [
             {
@@ -43,9 +44,7 @@ async def test_fetch_trivia_number_of_questions(httpx_mock):
                 "category": "General Knowledge",
                 "question": "Nutella is produced by the German company Ferrero.",
                 "correct_answer": "False",
-                "incorrect_answers": [
-                    "True"
-                ]
+                "incorrect_answers": ["True"],
             },
             {
                 "type": "boolean",
@@ -53,22 +52,24 @@ async def test_fetch_trivia_number_of_questions(httpx_mock):
                 "category": "General Knowledge",
                 "question": "March 10th is also known as Mar10 Day.",
                 "correct_answer": "True",
-                "incorrect_answers": [
-                    "False"
-                ]
-            }
-        ]
+                "incorrect_answers": ["False"],
+            },
+        ],
     }
-    httpx_mock.add_response(url="https://opentdb.com/api.php?amount=2&type=multiple&category=9", json=mock_response)
-    
+    httpx_mock.add_response(
+        url="https://opentdb.com/api.php?amount=2&type=multiple&category=9",
+        json=mock_response,
+    )
+
     service = TriviaService(base_url="https://opentdb.com/api.php")
     questions = await service.fetch_trivia(amount=2, type="multiple", category=9)
-    
+
     assert len(questions) == 2
+
 
 @pytest.mark.asyncio
 async def test_fetch_trivia_type_boolean(httpx_mock):
-    mock_response=  {
+    mock_response = {
         "response_code": 0,
         "results": [
             {
@@ -77,41 +78,44 @@ async def test_fetch_trivia_type_boolean(httpx_mock):
                 "category": "General Knowledge",
                 "question": "&quot;27 Club&quot; is a term used to refer to a list of famous actors, musicians, and artists who died at the age of 27.",
                 "correct_answer": "True",
-                "incorrect_answers": [
-                    "False"
-                ]
+                "incorrect_answers": ["False"],
             }
-        ]
+        ],
     }
-    httpx_mock.add_response(url="https://opentdb.com/api.php?amount=1&type=boolean&category=9", json=mock_response)
-    
+    httpx_mock.add_response(
+        url="https://opentdb.com/api.php?amount=1&type=boolean&category=9",
+        json=mock_response,
+    )
+
     service = TriviaService(base_url="https://opentdb.com/api.php")
     questions = await service.fetch_trivia(amount=1, type="boolean", category=9)
-    
+
     assert questions[0]["type"] == "boolean"
     assert questions[0]["correct_answer"] == "True"
     assert sorted(questions[0]["all_answers"]) == ["False", "True"]
 
+
 @pytest.mark.asyncio
 async def test_fetch_trivia_category(httpx_mock):
-    mock_response=  {
-    "response_code": 0,
-    "results": [
+    mock_response = {
+        "response_code": 0,
+        "results": [
             {
                 "type": "boolean",
                 "difficulty": "easy",
                 "category": "Entertainment: Film",
                 "question": "Actor Tommy Chong served prison time.",
                 "correct_answer": "True",
-                "incorrect_answers": [
-                    "False"
-                ]
+                "incorrect_answers": ["False"],
             }
-        ]
+        ],
     }
-    httpx_mock.add_response(url="https://opentdb.com/api.php?amount=1&type=multiple&category=11", json=mock_response)
-    
+    httpx_mock.add_response(
+        url="https://opentdb.com/api.php?amount=1&type=multiple&category=11",
+        json=mock_response,
+    )
+
     service = TriviaService(base_url="https://opentdb.com/api.php")
     questions = await service.fetch_trivia(amount=1, type="multiple", category=11)
-    
+
     assert questions[0]["category"] == "Entertainment: Film"
